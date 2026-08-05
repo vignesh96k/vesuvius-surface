be57af0ee7d142067df9737ed1802fcb476d12aa
vignesh96k <vigneshkannan1696@gmail.com>
2026-08-05 12:53:49 +0530

    Add missing nnU-Net export module

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -3,11 +3,6 @@
 from data.dataloader import build_dataloader, build_dataloaders_from_config
 from data.dataset import SegmentationDataset, SurfacePatchDataset, SurfaceVolumeDataset
 from data.io import VolumeRecord, build_volume_index, load_volume, read_metadata_csv
-from data.nnunet_export import (
-    NnUNetExportResult,
-    export_nnunet_dataset,
-    write_scroll_holdout_split,
-)
 from data.patching import PatchConfig, PatchConfig3D, PatchCoord3D, build_patch_index_3d, extract_patch_3d
 from data.schema import LABEL_BG, LABEL_IGNORE, LABEL_SURFACE, VALID_LABELS
 from data.transforms import build_transforms, normalize_volume
@@ -18,7 +13,6 @@ __all__ = [
     "LABEL_IGNORE",
     "LABEL_SURFACE",
     "VALID_LABELS",
-    "NnUNetExportResult",
     "PatchConfig",
     "PatchConfig3D",
     "PatchCoord3D",
@@ -32,11 +26,9 @@ __all__ = [
     "build_patch_index_3d",
     "build_transforms",
     "build_volume_index",
-    "export_nnunet_dataset",
     "extract_patch_3d",
     "load_volume",
     "normalize_volume",
     "read_metadata_csv",
     "validate_dataset",
-    "write_scroll_holdout_split",
 ]
